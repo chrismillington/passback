2a061518e0f33e12d77ce5a4023efbe40526a43c
Chris Millington <chris@themadllama.com>
2023-09-01 07:00:17 +0100

    Multiple

--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -3,18 +3,59 @@ import "./Parent.css";
 import { useState } from "react";
 
 const Child = (props) => {
-  const [text, setText] = useState("Random");
+  const [fName, setfName] = useState("Christian Name");
+  const [sName, setSName] = useState("Surname");
+  const [radio, setRadio] = useState("");
+
+  const update = () => {
+    props.changeFirst(fName);
+    props.changeSecond(sName);
+    props.changeLanguage(radio);
+  };
 
   return (
     <div className="Child">
       <h2>Child</h2>
-      <button onClick={() => props.changeWord(text)}>Hit me </button>
+      <button onClick={update}>Hit me </button>
+      {/* This double binds the fName state to the input */}
       <input
         type="text"
-        id="sprog"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
+        id="fName"
+        value={fName}
+        onChange={(e) => setfName(e.target.value)}
       />
+      <input
+        type="text"
+        id="sName"
+        value={sName}
+        onChange={(e) => setSName(e.target.value)}
+      />
+      <div className="radio">
+        <input
+          type="radio"
+          id="html"
+          name="fav_language"
+          value="HTML"
+          onChange={(e) => setRadio(e.target.value)}
+        />
+        <label for="html">HTML</label>
+        <input
+          type="radio"
+          id="css"
+          name="fav_language"
+          value="CSS"
+          onChange={(e) => setRadio(e.target.value)}
+        />
+        <label for="css">CSS</label>
+        <input
+          type="radio"
+          id="javascript"
+          name="fav_language"
+          value="JavaScript"
+          onChange={(e) => setRadio(e.target.value)}
+        />
+        <label for="javascript">JavaScript</label>
+      </div>
     </div>
   );
 };
