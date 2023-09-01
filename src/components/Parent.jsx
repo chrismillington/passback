8558fc9253a54759c15b0b2a425aa19e81ef1d80
Chris Millington <chris@themadllama.com>
2023-09-01 07:00:02 +0100

    First Complete

--- src/components/Parent.jsx
+++ src/components/Parent.jsx
@@ -3,12 +3,23 @@ import Child from "./Child";
 import "./Parent.css";
 
 const Parent = () => {
-  const [word, setWord] = useState("Parent");
-
+  const [word, setWord] = useState();
+  const [sName, setSName] = useState();
+  const [language, setLanguage] = useState("");
+  // https://blog.logrocket.com/guide-usestate-react/#:~:text=It%20returns%20an%20array%20with,setState%5D%20%3D%20useState(initialValue)%3B
   return (
     <div className="main">
-      <h1>{word}</h1>
-      <Child changeWord={(word) => setWord(word)} />
+      <h1>
+        {word ? word + " " + sName + " likes " + language : "Enter a value"}
+      </h1>
+      {/* - ChangeWord is recieved with parameters
+          - ChangeWord calls the setWord state
+      */}
+      <Child
+        changeFirst={(word) => setWord(word)}
+        changeSecond={(second) => setSName(second)}
+        changeLanguage={(language) => setLanguage(language)}
+      />
     </div>
   );
 };
